b0e360b98124fee84348e5046a78d04f2e9194ca
Khushi kumari Jha <jhakkhushi@gmail.com>
2025-10-26 19:30:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1189925023-c313f",
+  "appId": "1:372101324703:web:591e0bf3ab089be785e7aa",
+  "apiKey": "AIzaSyBDUD_447LdoYaIIgVcBMMBx5j85SJ4o8o",
+  "authDomain": "studio-1189925023-c313f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "372101324703"
+};
