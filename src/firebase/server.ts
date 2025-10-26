a239e71b6f4f06504826a8db213601518757ccfd
Khushi kumari Jha <jhakkhushi@gmail.com>
2025-10-26 19:36:49 +0000

    Try fixing this error: `Runtime Error: Error: Invalid Firebase app optio

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -2,16 +2,13 @@
 import { getApps, initializeApp, getApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { firebaseConfig } from './config';
 
 const apps = getApps();
 
-const app = apps.length
-  ? getApp()
-  : initializeApp({
-      credential: undefined, // Let ADC find the credential
-      projectId: firebaseConfig.projectId,
-    });
+// When no-args are provided, the SDK will look for credentials via ADC
+// or GOOGLE_APPLICATION_CREDENTIALS environment variables.
+// In a local emulated environment, it will connect to the emulators.
+const app = apps.length ? getApp() : initializeApp();
 
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
