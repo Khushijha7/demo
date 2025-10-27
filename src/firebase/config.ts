a5ba663b20c6fad8b0e6afa58a2f9319b3f56716
Khushi kumari Jha <jhakkhushi@gmail.com>
2025-10-27 07:43:55 +0000

    move all the keys to the .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "fintrack-project-9a74c.firebaseapp.com",
-  projectId: "fintrack-project-9a74c",
-  storageBucket: "fintrack-project-9a74c.firebasestorage.app",
-  messagingSenderId: "445433532739",
-  appId: "1:445433532739:web:3a25264ad77ab4a6ba9ff6",
-  measurementId: "G-9LMB2PLZMW"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
