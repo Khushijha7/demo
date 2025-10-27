99482efee398d280ad63291d0408d50d3ff19e7c
Khushi kumari Jha <jhakkhushi@gmail.com>
2025-10-27 07:15:59 +0000

    make all page width to 1280px

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -27,7 +27,7 @@ export default function DashboardLayout({
         <AppSidebar />
         <div className="flex-1">
           <AppHeader />
-          <main className="p-4 sm:p-6 lg:p-8">{children}</main>
+          <main className="p-4 sm:p-6 lg:p-8 max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </div>
     </SidebarProvider>
